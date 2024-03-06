ef89f13f293d0ee75b2420273c72e2ad1d0e5151
Husein Mehmedagic <husein.mehmedagic@gmail.com>
2024-03-06 06:58:29 +0100

    First commit

--- src/calculator.py
+++ src/calculator.py
@@ -3,4 +3,5 @@
 def add(first_num, second_num):
   return first_num + second_num
 
-  #return first_num + second_num
+def substract(first_num, second_num):
+  return first_num - second_num
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,4 +1,8 @@
 import pytest
-from src.calculator import add
+from src.calculator import add, substract
+
 def test_add():
   assert add(3, 4) == 7
+
+def test_substract():
+  assert substract(5, 3) == 2
